cb8bc16745f95e4b6fff43a8b0f00cd18aa7bc00
caorsi <caorsi@DESKTOP-G0V9HAL>
2025-03-14 16:04:07 -0300

    Add project files.

---
+++ orbitalSim.h
@@ -0,0 +1,44 @@
+﻿/**
+ * @brief Orbital simulation
+ * @author Marc S. Ressl
+ *
+ * @copyright Copyright (c) 2022-2023
+ */
+
+#ifndef ORBITALSIM_H
+#define ORBITALSIM_H
+#include <raylib.h>
+/**
+ * @brief Orbital body definition
+ */
+struct OrbitalBody
+{
+    // Fill in your code here...
+    float mass;		  // [kg]
+    float radius;	  // [m]
+    Color color;	  // Raylib color
+    Vector3 position; // [m]
+    Vector3 velocity; // [m/s]
+    Vector3 aceleration; // [m/s^2]
+
+};
+
+/**
+ * @brief Orbital simulation definition
+ */
+struct OrbitalSim
+{
+    // Fill in your code here...
+    float timeStep;
+    float elapsedTime;
+    unsigned int numBodies;
+    OrbitalBody bodies[50];     //Place a define so that the length of the array is well determined
+
+};
+
+OrbitalSim *constructOrbitalSim(float timeStep);
+void destroyOrbitalSim(OrbitalSim *sim);
+
+void updateOrbitalSim(OrbitalSim *sim);
+
+#endif
